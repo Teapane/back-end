c2ae69b632c5b788325c7c19214f92dbafb0d603
Norma Lopez <norma_l304@yahoo.com>
2021-01-05 11:19:20 -0700

    Remove email from vendor model
    Co-authored-by: Tyler Fields <tdfields93@gmail.com>

--- app/core/models.py
+++ app/core/models.py
@@ -3,9 +3,8 @@ from django.db import models
 
 class Vendor(models.Model):
     name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str(self):
+    def __str__(self):
         return self.name
