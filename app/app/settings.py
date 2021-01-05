8de9c2b9d630cce23722063b6ddd71d379c50a9b
Norma Lopez <norma_l304@yahoo.com>
2021-01-04 17:33:23 -0700

    Update databases to include name, user, password, host, port
    Co-authored-by: Tyler Fields <tdfields93@gmail.com>

--- app/app/settings.py
+++ app/app/settings.py
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'PORT': os.environ.get('DB_PORT'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
