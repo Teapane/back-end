bb9ad487cd894902ec8d2b4081c22e3c00ed2309
Norma Lopez <norma_l304@yahoo.com>
2021-01-06 11:57:10 -0700

    Add test for retrieving all weddings api
    Co-authored-by: Tyler Fields <tdfields93@gmail.com>

---
+++ app/wedding/tests/test_wedding_api.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+from core.models import Wedding
+
+from wedding.serializers import WeddingSerializer
+
+WEDDINGS_URL = reverse('get_post_weddings')
+
+
+class GetAllWeddingsTests(TestCase):
+    """Test module for GET all weddings API"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+        Wedding.objects.create(
+        name='Simpsons',
+        email='test@email.com',
+        date='06/01/2021'
+        )
+        Wedding.objects.create(
+        name='Smith',
+        email='test2@email.com',
+        date='08/01/2021'
+        )
+
+    def test_retrieve_wedding_list(self):
+        response = client.get(WEDDINGS_URL)
+        wedding = Wedding.objects.all()
+        serializer = WeddingSerializer(weddings, many=True)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
