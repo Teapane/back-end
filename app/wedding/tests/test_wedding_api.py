72f0e2899417038d772e35bab5f4e59255943e4a
Norma Lopez <norma_l304@yahoo.com>
2021-01-06 15:18:14 -0700

    Add test for retrieving all weddings
    Co-authored-by: Tyler Fields <tdfields93@gmail.com>

--- app/wedding/tests/test_wedding_api.py
+++ app/wedding/tests/test_wedding_api.py
@@ -8,7 +8,7 @@ from core.models import Wedding
 
 from wedding.serializers import WeddingSerializer
 
-WEDDINGS_URL = reverse('get_post_weddings')
+WEDDINGS_URL = reverse('wedding:wedding-list')
 
 
 class GetAllWeddingsTests(TestCase):
@@ -29,8 +29,8 @@ class GetAllWeddingsTests(TestCase):
         )
 
     def test_retrieve_wedding_list(self):
-        response = client.get(WEDDINGS_URL)
-        wedding = Wedding.objects.all()
+        response = self.client.get(WEDDINGS_URL)
+        weddings = Wedding.objects.all()
         serializer = WeddingSerializer(weddings, many=True)
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
