832576164f2a748129acaa30683c73a1b0ea6e4c
fieldstyler <tdfields93@gmail.com>
2021-01-05 17:00:36 -0700

    Create tests for PhotoGuest and WeddingGuest with corresponding models and migrations
    Co-authored-by: Norma Lopez <norma_l304@yahoo.com>

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,10 +8,28 @@ class ModelTests(TestCase):
         self.wedding1 = models.Wedding.objects.create(
             name = 'Bart',
             email = 'test@email.com',
-            date = 'October 10th, 2022',
-            image = 'smthg.jpg'
+            date = 'October 10th, 2022'
         )
 
+        self.guest1 = models.Guest.objects.create(
+            name='Marge',
+            phone_number='1234567890',
+            wedding=self.wedding1
+        )
+
+        self.photo1 = models.Photo.objects.create(
+            number='1'
+        )
+
+        self.photoguest1 = models.PhotoGuest.objects.create(
+            guest=self.guest1,
+            photo=self.photo1
+        )
+
+        self.weddingguest1 = models.WeddingGuest.objects.create(
+            wedding=self.wedding1,
+            guest=self.guest1
+        )
 
     def test_wedding_str(self):
         """Test the wedding string representation"""
@@ -30,3 +48,21 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(guest), guest.name)
+
+
+    def test_photo_str(self):
+        """Test the photo string representation"""
+        self.assertEqual(str(self.photo1), self.photo1.number)
+
+
+
+    def test_photoguest_attrs(self):
+        """Test the photoguest attribute representations"""
+        self.assertEqual((self.photoguest1.photo), self.photo1)
+        self.assertEqual((self.photoguest1.guest), self.guest1)
+
+
+    def test_wedding_guest_attrs(self):
+        """Test the photoguest attribute representations"""
+        self.assertEqual((self.weddingguest1.wedding), self.wedding1)
+        self.assertEqual((self.weddingguest1.guest), self.guest1)
