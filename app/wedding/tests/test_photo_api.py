ef18914a73b2b36843ffcb4e11b87db4574bbd69
Norma Lopez <norma_l304@yahoo.com>
2021-01-11 19:45:40 -0700

    Add test for photo wedding
    Co-authored-by: Tyler Fields <tdfields93@gmail.com>

--- app/wedding/tests/test_photo_api.py
+++ app/wedding/tests/test_photo_api.py
@@ -6,6 +6,9 @@ from rest_framework import status
 
 from core.models import Wedding
 from core.models import Guest
+from core.models import Photo
+
+import code
 
 CREATE_PHOTO_URL = reverse('wedding:create_photo')
 
@@ -35,9 +38,68 @@ class PostPhotoTests(TestCase):
         photo_data = {
             'number': '1',
             'description': 'Bride with Groom Family',
-            'guest': guest.id
+            'guest': guest.id,
+            'wedding': wedding.id
         }
 
         res = self.client.post(CREATE_PHOTO_URL, photo_data)
 
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+    def test_get_all_photos_given_wedding_id(self):
+        wedding1 = Wedding(
+                        name='Simpsons',
+                        email='simpsonwed@gmail.com',
+                        date='2345675432'
+                        )
+        wedding1.save()
+
+        wedding2 = Wedding.objects.create(
+            name='Bart',
+            email='test@email.com',
+            date='October 10th, 2022'
+        )
+
+        guest1 = Guest.objects.create(
+                        name='Uncle Bob',
+                        phoneNumber='2343454567',
+                        wedding=wedding1
+        )
+
+        guest2 = Guest.objects.create(
+            name='Aunt Joan',
+            phoneNumber='3450962384',
+            wedding=wedding1
+        )
+
+        guest3 = Guest.objects.create(
+            name='Grandma Charlotte',
+            phoneNumber='34523462384',
+            wedding=wedding2
+        )
+
+        photo1 = Photo.objects.create(
+                        number='5',
+                        description="Bride's family",
+                        wedding=wedding1
+        )
+
+        photo1.guest.set([guest1, guest2])
+
+        photo2 = Photo.objects.create(
+                        number='6',
+                        description="Friends",
+                        wedding=wedding2
+        )
+
+        photo2.guest.set([guest3])
+
+        res = self.client.get(
+                    "/api/v1/weddings/photos/?wedding=% s" % wedding1.id
+                    )
+
+        code.interact(local=locals())
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn(photo1.number, res.data[0]['number'])
+        self.assertNotIn(photo2.number, res.data[0]['number'])
+        
