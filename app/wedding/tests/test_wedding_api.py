960b3a5c76f4c5a984de9305dda3898b2814f4dc
fieldstyler <tdfields93@gmail.com>
2021-01-12 14:24:30 -0700

    Update delete wedding test to be more robust
    Co-authored-by: Norma Lopez <norma_l304@yahoo.com>

--- app/wedding/tests/test_wedding_api.py
+++ app/wedding/tests/test_wedding_api.py
@@ -8,11 +8,8 @@ from core.models import Wedding
 
 from wedding.serializers import WeddingSerializer
 
-import code
-
 WEDDINGS_URL = reverse('wedding:wedding-list')
 CREATE_WEDDING_URL = reverse('wedding:create')
-DELETE_WEDDING_URL = reverse('wedding:delete')
 
 
 class WeddingTests(TestCase):
@@ -54,15 +51,32 @@ class WeddingTests(TestCase):
 
     def test_delete_wedding_instance(self):
 
-        wedding_data = {
+        wedding_data1 = {
             'name': 'Simpsons',
             'email': 'test@email.com',
             'date': '06/01/2021'
         }
 
-        res = self.client.post(CREATE_WEDDING_URL, wedding_data)
+        wedding_data2 = {
+            'name': 'Griffins',
+            'email': 'test@email.com',
+            'date': '11/22/2022'
+        }
+
+        res = self.client.post(CREATE_WEDDING_URL, wedding_data1)
+        res2 = self.client.post(CREATE_WEDDING_URL, wedding_data2)
 
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
-        wedding = Wedding.objects.all()
-        code.interact(local=locals())
-        response = self.client.delete(DELETE_WEDDING_URL)
+        self.assertEqual(res2.status_code, status.HTTP_201_CREATED)
+
+        weddings = Wedding.objects.all()
+        response = self.client.get(
+                "/api/v1/weddings/remove/?wedding=% s" % weddings[0].id
+                                )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response2 = self.client.get(WEDDINGS_URL)
+        serializer = WeddingSerializer(weddings, many=True)
+        self.assertEqual(response2.data, serializer.data)
+        self.assertEqual(response2.status_code, status.HTTP_200_OK)
+        self.assertIn(weddings[0].name, response2.data[0]['name'])
